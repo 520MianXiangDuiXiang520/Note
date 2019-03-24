09f8112de9b1d66de07d1625a5c807a563bc9920
520MianXiangDuiXiang520 <15364968962@163.com>
2019-03-24 20:34:21 +0800

    杂七杂八

---
+++ 蓝桥杯/01图形显示.cpp
@@ -0,0 +1,24 @@
+﻿// 图形显示.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,i,j;
+	cin >> n;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
