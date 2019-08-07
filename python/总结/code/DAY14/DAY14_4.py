5689343d3ef9189ce234d982ba376cba7b82d0c3
520MianXiangDuiXiang520 <15364968962@163.com>
2019-08-07 11:48:43 +0800

    ubuntu

--- python/总结/code/DAY14/DAY14_4.py
+++ python/总结/code/DAY14/DAY14_4.py
@@ -1,5 +1,24 @@
-install threading
+import threading
 
+NUM = 0
 
 def Foo1(num: int):
+    global NUM
+    for i in range(num):
+        NUM += 1
+
+def Foo2(num: int):
+    global NUM
+    for i in range(num):
+        NUM += 1
+
+def main():
+    t1 = threading.Thread(target=Foo1,args=(1000,))
+    t2 = threading.Thread(target=Foo2,args=(1000,))
+    t1.start()
+    t2.start()
+
+
+if __name__ == '__main__':
+    main()
     
